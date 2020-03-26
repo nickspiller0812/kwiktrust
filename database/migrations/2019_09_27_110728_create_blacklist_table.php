a30ecd914b8fc085182cd8d0ccdc09ab47f8c397
nickspiller0812 <nickspiller0812@gmail.com>
2020-03-26 19:46:13 +0000

    Add files via upload

---
+++ database/migrations/2019_09_27_110728_create_blacklist_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlacklistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blacklists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email_suffix');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blacklist');
+    }
+}
